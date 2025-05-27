8d9d6ad5b93f6eb829529391c9cb74c76dadbf8d
Amon Victor <amon.victor.develops@gmail.com>
2025-05-27 20:46:33 +0300

    Added code to Convert Country Codes to Full Names

--- checker.js
+++ checker.js
@@ -7,6 +7,7 @@ const API_KEY = `9785b5daa415b3de7d2cd8e201f92359`
 checkBtn.addEventListener("click", async function(e) {
     e.preventDefault();
     let CheckValue = checkInput.value
+    resultsDiv.innerHTML = ""
     
     //input validation
     if(CheckValue === "") {
@@ -16,17 +17,16 @@ checkBtn.addEventListener("click", async function(e) {
 
     try {
         const response = await fetch(`https://api.nationalize.io/?name=${CheckValue}&apiKey=${API_KEY}`)
-        const results = await response.json();
-        console.log(results);
-        
+        const results = await response.json();      
         let {name, country} = results;
         
         country.forEach(nationalityList => {
             let {country_id, probability} = nationalityList
+            const countryName = convertCodeToCountry(country_id)
             let resultContent = `
                 <div id="nationalityItem" class="nationalityItem">
                     <div class="probabilities" id="probabilities">
-                        <div class="country" id="country">${country_id}</div>
+                        <div class="country" id="country">${country_id} (${countryName})</div>
                         <div class="probability" id="probability">${probability.toFixed(4) * 100}%</div>
                     </div>
                 </div>
@@ -34,10 +34,13 @@ checkBtn.addEventListener("click", async function(e) {
             resultsDiv.innerHTML += resultContent
         });
 
-
-
     } catch (error) {
         throw new Error(error)
     }
 
 })
+
+function convertCodeToCountry(countryCode) {
+    const regionNames = new Intl.DisplayNames(['en'], {type: 'region'})
+    return regionNames.of(countryCode)
+}
